c8fb5e12e03e0713d4e7c0f7e0f4d54b5aab8be1
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-06-04 22:13:11 -0500

    Adding changes components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({ id, name, email}) => {
-	return (
-		<div className="bg-light-blue dib br3 pa3 ma2 grow tc">
-			<img src={`https://www.robohash.org/${id}?size=200x200`} alt="robots" />
-			<div>
-				<h2>{name}</h2>
-				<p>{email}</p>
-			</div>
-		</div>
-		);
-}
+const Card = ({ id, name, email }) => {
+  return (
+    <div className="bg-light-blue dib br3 pa3 ma2 grow tc">
+      <img src={`https://www.robohash.org/${id}?size=200x200`} alt="robots" SameSite={`None`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Scroll = (props) => {
-	return (
-		<div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'  }}>
-			{props.children}
-		</div>
-	);
+  return (
+    <div
+      style={{ overflow: "scroll", border: "5px solid black", height: "800px" }}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Scroll;
